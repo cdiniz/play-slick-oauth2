464055bc590e006a880ce1e889fbff71c3979004
Cláudio Diniz <cdiniz@equalexperts.com>
2016-06-03 18:56:14 +0100

    Add tests for Oauth Controller

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,34 +1,63 @@
 import org.scalatestplus.play._
-import play.api.libs.json.JsObject
+import play.api.Application
+import play.api.libs.json.{JsObject, Json, Reads}
 import play.api.test._
 import play.api.test.Helpers._
 
 
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
+  case class TokenResponse(token_type : String, access_token : String, expires_in : Int, refresh_token : String)
+  implicit val tokenReader : Reads[TokenResponse] = Json.reads[TokenResponse]
+
+  case class TokenErrorResponse(error : String, error_description : String)
+  implicit val TokenErrorResponseReader : Reads[TokenErrorResponse] = Json.reads[TokenErrorResponse]
+
   "Routes" should {
 
-    "send 404 on a bad request" in  {
+    "send 404 on a bad request" in {
       route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
     }
 
   }
 
-    "OAuthController" should {
+  "OAuthController" should {
 
-      "return unauthorized when trying to access resources without token" in {
-        val resources = route(app, FakeRequest(GET, "/resources")).get
+    "return unauthorized when trying to access resources without token" in {
+      val resources = route(app, FakeRequest(GET, "/resources")).get
 
-        status(resources) mustBe BAD_REQUEST
-      }
+      status(resources) mustBe BAD_REQUEST
+    }
 
-      "return unauthorized when trying to access resources without a valid token" in {
-        val resources = route(app, FakeRequest(GET, "/resources",FakeHeaders(
-          ("Content-type","application/json") :: ("Authorization","Bearer faketoken") :: Nil),JsObject(Seq()))).get
+    "return unauthorized when trying to access resources without a valid token" in {
+      val resources = route(app, FakeRequest(GET, "/resources", FakeHeaders(
+        ("Content-type", "application/json") ::("Authorization", "Bearer faketoken") :: Nil), JsObject(Seq()))).get
 
-        status(resources) mustBe UNAUTHORIZED
-      }
+      status(resources) mustBe UNAUTHORIZED
+    }
+
+    "return an error with invalid client when try to create a token with invalid credentials" in {
+      val resources = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "wrong id", "client_secret" -> "wrong secret", "grant_type" -> "client_credentials")).get
+
+      status(resources) mustBe UNAUTHORIZED
+
+      val tokenErrorResponse = contentAsJson(resources).as[TokenErrorResponse]
+
+      tokenErrorResponse.error mustBe "invalid_client"
 
     }
+
+    "return a valid auth token and refresh token with valid credentials" in {
+      val resources = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id", "client_secret" -> "bob_client_secret", "grant_type" -> "client_credentials")).get
+
+      status(resources) mustBe OK
+
+      val tokenResponse = contentAsJson(resources).as[TokenResponse]
+
+      tokenResponse.token_type mustBe "Bearer"
+      tokenResponse.access_token must not be(tokenResponse.refresh_token)
+    }
+
+  }
 
 }
