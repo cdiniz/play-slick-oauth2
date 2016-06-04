7f5f439df3043d88242e85fd8502c631e3f297f9
Cláudio Diniz <cdiniz@equalexperts.com>
2016-06-04 11:04:47 +0100

    Add tests for token refresh

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -48,14 +48,55 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
     }
 
     "return a valid auth token and refresh token with valid credentials" in {
-      val resources = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id", "client_secret" -> "bob_client_secret", "grant_type" -> "client_credentials")).get
+      val tokenResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id",
+        "client_secret" -> "bob_client_secret", "grant_type" -> "client_credentials")).get
 
-      status(resources) mustBe OK
+      status(tokenResp) mustBe OK
 
-      val tokenResponse = contentAsJson(resources).as[TokenResponse]
+      val tokenResponse = contentAsJson(tokenResp).as[TokenResponse]
 
       tokenResponse.token_type mustBe "Bearer"
       tokenResponse.access_token must not be(tokenResponse.refresh_token)
+    }
+
+    "return error when try to refresh the token with an invalid token" in {
+      val tokenResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id",
+        "client_secret" -> "bob_client_secret", "grant_type" -> "client_credentials")).get
+      status(tokenResp) mustBe OK
+      val tokenResponse = contentAsJson(tokenResp).as[TokenResponse]
+
+      val refreshResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id", "client_secret" -> "bob_client_secret",
+        "refresh_token" -> "Invalid token", "grant_type" -> "refresh_token")).get
+
+      val refreshResponse = contentAsJson(refreshResp).as[TokenErrorResponse]
+      refreshResponse.error mustBe "invalid_request"
+    }
+
+    "return error when try to refresh the token with a valid token but wrong credentials" in {
+      val tokenResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id",
+        "client_secret" -> "bob_client_secret", "grant_type" -> "client_credentials")).get
+      status(tokenResp) mustBe OK
+      val tokenResponse = contentAsJson(tokenResp).as[TokenResponse]
+
+      val refreshResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id", "client_secret" -> "Invalid bob_client_secret",
+        "refresh_token" -> tokenResponse.refresh_token, "grant_type" -> "refresh_token")).get
+
+      val refreshResponse = contentAsJson(refreshResp).as[TokenErrorResponse]
+      refreshResponse.error mustBe "invalid_client"
+    }
+
+    "return a new token after refresh" in {
+      val tokenResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id",
+        "client_secret" -> "bob_client_secret", "grant_type" -> "client_credentials")).get
+      status(tokenResp) mustBe OK
+      val tokenResponse = contentAsJson(tokenResp).as[TokenResponse]
+
+      val refreshResp = route(app, FakeRequest(POST, "/oauth/access_token").withFormUrlEncodedBody("client_id" -> "bob_client_id", "client_secret" -> "bob_client_secret",
+        "refresh_token" -> tokenResponse.refresh_token, "grant_type" -> "refresh_token")).get
+
+      val refreshResponse = contentAsJson(refreshResp).as[TokenResponse]
+      refreshResponse.token_type mustBe "Bearer"
+      refreshResponse.access_token must not be(tokenResponse.refresh_token)
     }
 
   }
